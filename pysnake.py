27f54a84dca645019e0daafd1b57b5e029ea3fd0
Ankith <46915066+ankith26@users.noreply.github.com>
2019-11-14 21:27:44 +0530

    Update pysnake.py

--- pysnake.py
+++ pysnake.py
@@ -3,6 +3,7 @@ import random
 pygame.init()
 clock = pygame.time.Clock()
 def newfood():
+    # just generates position of food
     if borders:
         cord = [random.randint(2,34), random.randint(2,34)]
     else:
@@ -14,12 +15,18 @@ def newfood():
 win = pygame.display.set_mode((360, 360))
 pygame.display.set_caption('Snake')
 running = True
+# some initialisation
 array = [[2,10]]
 food = [random.randint(1,36), random.randint(1,36)]
 key = "right"
+### Change this setting to apply borders ###
+### it can be either True or False ###
 borders = True
+
+# main loop
 while running:
     clock.tick(5)
+    # get all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT: running = False
         elif event.type == pygame.KEYDOWN:
@@ -36,6 +43,7 @@ while running:
                 if key != "up":
                     key = "down"
         else: pass
+    # get direction based on key events
     if key == "up":
         pos = [array[0][0], array[0][1]-1]
     elif key == "down":
@@ -46,15 +54,21 @@ while running:
         pos = [array[0][0]-1, array[0][1]]
     else:
         break
+    # handle border cases
     if borders:
+        # If borders enabled,
+        # If you crash into them, then gameover
         if pos[0] not in range(2,36) or pos[1] not in range(2,36):
             running = False
             print("Gameover")
+        # Draw the borders
         pygame.draw.rect(win, (255,0,255),(0,0,10,360))
         pygame.draw.rect(win, (255,0,255),(0,0,360,10))
         pygame.draw.rect(win, (255,0,255),(350,0,10,360))
         pygame.draw.rect(win, (255,0,255),(0,350,360,10))
     else:
+        # If borders are disabled,
+        # The snake can pop from other side of the screen
         if pos[0] < 2:
             pos[0] = 36
         elif pos[0] > 36:
@@ -63,16 +77,19 @@ while running:
             pos[1] = 36
         elif pos[1] > 36:
             pos[1] = 1
+    # Eat the food and generate new food after eating
     if pos == food:
         array.append(array[-1])
         food = newfood()
-    
+    # check wether it collides with itself
     if array.count(pos) >= 1:
         running = False
         print("Gameover")
     else:
+        # else continue by moving a step further
         array.insert(0, pos)
         cords = array.pop()
+        # draw all the stuff
         pygame.draw.rect(win, (255,255,0),(food[0]*10-10,food[1]*10-10,10,10))
         pygame.draw.rect(win, (0,0,0),(cords[0]*10-10,cords[1]*10-10,10,10))
         pygame.draw.rect(win, (255,255,255),(pos[0]*10-10,pos[1]*10-10,10,10))
